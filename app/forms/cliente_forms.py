05fa4cc77381f2045e031a40971137181a21f697
ITALORAMONLC <italo.ramon2020@gmail.com>
2023-07-06 21:16:35 -0300

    first commit

---
+++ app/forms/cliente_forms.py
@@ -0,0 +1,14 @@
+from django.forms import ModelForm
+from app.models import Cliente,Endereco
+
+
+class ClienteForm(ModelForm):
+    class Meta:
+        model = Cliente
+        exclude = ("endereco",)
+
+
+class EnderecoForm(ModelForm):
+    class Meta:
+        model = Endereco
+        fields = "__all__"
